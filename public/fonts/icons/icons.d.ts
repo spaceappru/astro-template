989ef51996ea2b1aab49565918f23ee9fd51bb5b
Aleksei Epifantsev <alex@lahtihosting.fi>
2025-05-05 01:26:15 +0400

    feat: new icons

---
+++ public/fonts/icons/icons.d.ts
@@ -0,0 +1,15 @@
+export enum Icons {
+  Attachment = "icon-attachment",
+  Burger = "icon-burger",
+  Chevron = "icon-chevron",
+  Clear = "icon-clear",
+  Cross = "icon-cross",
+  Mail = "icon-mail",
+  Phone = "icon-phone",
+  Telegram = "icon-telegram",
+  Vk = "icon-vk",
+  Whatsapp = "icon-whatsapp",
+}
+export type IconsClassname = "icon-attachment" | "icon-burger" | "icon-chevron" | "icon-clear" | "icon-cross" | "icon-mail" | "icon-phone" | "icon-telegram" | "icon-vk" | "icon-whatsapp"
+export type IconsIcon = "attachment" | "burger" | "chevron" | "clear" | "cross" | "mail" | "phone" | "telegram" | "vk" | "whatsapp"
+export const IconsPrefix = "icon-"
